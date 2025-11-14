070ed96235f6d7deaab39a05b41e81398d205e1b
Andrew William Watson <aww@awwsmm.com>
2025-11-14 16:58:46 -0400

    correctly parse file containing whitespace with csv and serde

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
-use std::fs;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Transaction {
+    // note, there are a few ways around the below, we could also use a raw identifier like r#type
+    #[serde(rename = "type")] kind: String,
+    client: u16,
+    tx: u32,
+    amount: f32, // TODO replace this with a proper currency type
+}
 
 fn main() {
     let filename = std::env::args_os().nth(1).expect("missing file argument");
     println!("attempting to read file: {:?}", filename);
 
-    match fs::read_to_string(filename) {
-        Ok(file) => {
-            println!("read file contents: {}", file)
-        }
+    let reader = csv::ReaderBuilder::new()
+        .trim(csv::Trim::All) // trim all whitespace
+        .has_headers(true) // file has a header row
+        .from_path(filename);
+
+    let mut reader = match reader {
+        Ok(reader) => reader,
         Err(error) => {
             // in the "thousands of concurrent connections" scenario
             // this should not panic, but instead write an error to a logfile
             panic!("error reading file: {}", error)
         }
-    }
+    };
 
+    for result in reader.deserialize() {
+        let transaction: Transaction = result.expect("could not parse line");
+        println!("{:?}", transaction);
+    }
 }
