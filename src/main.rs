1b5b02e86ddf7c9f24ae8d3d88a37be39d13b136
Andrew William Watson <aww@awwsmm.com>
2025-11-14 16:22:32 -0400

    simple file reading and contents echoing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = std::env::args_os().nth(1).expect("missing file argument");
+    println!("attempting to read file: {:?}", filename);
+
+    match fs::read_to_string(filename) {
+        Ok(file) => {
+            println!("read file contents: {}", file)
+        }
+        Err(error) => {
+            // in the "thousands of concurrent connections" scenario
+            // this should not panic, but instead write an error to a logfile
+            panic!("error reading file: {}", error)
+        }
+    }
+
 }
